543fa9af8969cb0de42a7b4704ef015a9541bcce
Cristian Incarnato <cristian.cdi@gmail.com>
2020-10-08 19:57:57 -0300

    Add lib

---
+++ packages/logger/logger-backend/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.setupDefaultLogger = void 0;
+
+const setupDefaultLogger = require("./setupDefaultLogger");
+
+exports.setupDefaultLogger = setupDefaultLogger;
