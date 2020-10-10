8db0fcb650b68b2682810b3c3f5923e6341ed39a
Cristian Incarnato <cristian.cdi@gmail.com>
2020-10-09 23:52:29 -0300

    DefaultLogger

--- packages/logger/logger-backend/lib/index.js
+++ packages/logger/logger-backend/lib/index.js
@@ -1,10 +1,65 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.setupDefaultLogger = void 0;
+var _CustomJsonFormatter = _interopRequireDefault(require("./formatters/CustomJsonFormatter"));
 
-const setupDefaultLogger = require("./setupDefaultLogger");
+var _DefaultFormatter = _interopRequireDefault(require("./formatters/DefaultFormatter"));
 
-exports.setupDefaultLogger = setupDefaultLogger;
+var _DefaultJsonFormatter = _interopRequireDefault(require("./formatters/DefaultJsonFormatter"));
+
+var _DefaultTextFormatter = _interopRequireDefault(require("./formatters/DefaultTextFormatter"));
+
+var _GqlErrorLog = _interopRequireDefault(require("./helpers/GqlErrorLog"));
+
+var _GqlResponseLog = _interopRequireDefault(require("./helpers/GqlResponseLog"));
+
+var _DefaultLogger = _interopRequireDefault(require("./loggers/DefaultLogger"));
+
+var _GqlErrorLogger = _interopRequireDefault(require("./loggers/GqlErrorLogger"));
+
+var _GqlResponseLogger = _interopRequireDefault(require("./loggers/GqlResponseLogger"));
+
+var _RequestLogger = _interopRequireDefault(require("./loggers/RequestLogger"));
+
+var _RequestMiddleware = _interopRequireDefault(require("./middlewares/RequestMiddleware"));
+
+var _ConsoleTransport = _interopRequireDefault(require("./transports/ConsoleTransport"));
+
+var _FileAccessTransport = _interopRequireDefault(require("./transports/FileAccessTransport"));
+
+var _FileCombinedTransport = _interopRequireDefault(require("./transports/FileCombinedTransport"));
+
+var _FileErrorTransport = _interopRequireDefault(require("./transports/FileErrorTransport"));
+
+var _FileGqlErrorTransport = _interopRequireDefault(require("./transports/FileGqlErrorTransport"));
+
+var _FileGqlResponseTransport = _interopRequireDefault(require("./transports/FileGqlResponseTransport"));
+
+var _ResponseTimeMiddleware = _interopRequireDefault(require("./middlewares/ResponseTimeMiddleware"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = {
+  //Formatters
+  CustomJsonFormatter: _CustomJsonFormatter.default,
+  DefaultFormatter: _DefaultFormatter.default,
+  DefaultJsonFormatter: _DefaultJsonFormatter.default,
+  DefaultTextFormatter: _DefaultTextFormatter.default,
+  //Helpers
+  GqlErrorLog: _GqlErrorLog.default,
+  GqlResponseLog: _GqlResponseLog.default,
+  //Loggers
+  DefaultLogger: _DefaultLogger.default,
+  GqlErrorLogger: _GqlErrorLogger.default,
+  GqlResponseLogger: _GqlResponseLogger.default,
+  RequestLogger: _RequestLogger.default,
+  //Middlewares
+  ResponseTimeMiddleware: _ResponseTimeMiddleware.default,
+  RequestMiddleware: _RequestMiddleware.default,
+  //Transports
+  ConsoleTransport: _ConsoleTransport.default,
+  FileAccessTransport: _FileAccessTransport.default,
+  FileCombinedTransport: _FileCombinedTransport.default,
+  FileErrorTransport: _FileErrorTransport.default,
+  FileGqlErrorTransport: _FileGqlErrorTransport.default,
+  FileGqlResponseTransport: _FileGqlResponseTransport.default
+};
