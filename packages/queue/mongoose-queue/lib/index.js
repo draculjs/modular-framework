6d16c5be222f3de75d9f7d0df2320080857255f2
Cristian Incarnato <cristian.cdi@gmail.com>
2020-10-30 17:30:07 -0300

    Downgrade mongoose.

--- packages/queue/mongoose-queue/lib/index.js
+++ packages/queue/mongoose-queue/lib/index.js
@@ -3,7 +3,108 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+Object.defineProperty(exports, "MongooseQueue", {
+  enumerable: true,
+  get: function () {
+    return _mongooseQueue.default;
+  }
+});
+Object.defineProperty(exports, "QueueModel", {
+  enumerable: true,
+  get: function () {
+    return _QueueModel.default;
+  }
+});
+Object.defineProperty(exports, "QueueStatsModel", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsModel.default;
+  }
+});
+Object.defineProperty(exports, "producerManager", {
+  enumerable: true,
+  get: function () {
+    return _producerManager.default;
+  }
+});
+Object.defineProperty(exports, "workerManager", {
+  enumerable: true,
+  get: function () {
+    return _workerManager.default;
+  }
+});
+Object.defineProperty(exports, "fetchQueues", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.fetchQueues;
+  }
+});
+Object.defineProperty(exports, "addJob", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.addJob;
+  }
+});
+Object.defineProperty(exports, "ackJob", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.ackJob;
+  }
+});
+Object.defineProperty(exports, "errorJob", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.errorJob;
+  }
+});
+Object.defineProperty(exports, "getJob", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.getJob;
+  }
+});
+Object.defineProperty(exports, "cleanQueue", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.cleanQueue;
+  }
+});
+Object.defineProperty(exports, "resetQueue", {
+  enumerable: true,
+  get: function () {
+    return _QueueService.resetQueue;
+  }
+});
+Object.defineProperty(exports, "fetchQueueStats", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsService.fetchQueueStats;
+  }
+});
+Object.defineProperty(exports, "incrementAddedStat", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsService.incrementAddedStat;
+  }
+});
+Object.defineProperty(exports, "incrementGottenStat", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsService.incrementGottenStat;
+  }
+});
+Object.defineProperty(exports, "incrementFailedStat", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsService.incrementFailedStat;
+  }
+});
+Object.defineProperty(exports, "incrementDoneStat", {
+  enumerable: true,
+  get: function () {
+    return _QueueStatsService.incrementDoneStat;
+  }
+});
 
 var _mongooseQueue = _interopRequireDefault(require("./mongoose-queue"));
 
@@ -20,26 +121,3 @@ var _QueueService = require("./services/QueueService");
 var _QueueStatsService = require("./services/QueueStatsService");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var _default = {
-  //Queue Services
-  fetchQueues: _QueueService.fetchQueues,
-  addJob: _QueueService.addJob,
-  ackJob: _QueueService.ackJob,
-  errorJob: _QueueService.errorJob,
-  getJob: _QueueService.getJob,
-  cleanQueue: _QueueService.cleanQueue,
-  resetQueue: _QueueService.resetQueue,
-  //Queue Stat Services
-  fetchQueueStats: _QueueStatsService.fetchQueueStats,
-  incrementAddedStat: _QueueStatsService.incrementAddedStat,
-  incrementGottenStat: _QueueStatsService.incrementGottenStat,
-  incrementFailedStat: _QueueStatsService.incrementFailedStat,
-  incrementDoneStat: _QueueStatsService.incrementDoneStat,
-  workerManager: _workerManager.default,
-  producerManager: _producerManager.default,
-  QueueStatsModel: _QueueStatsModel.default,
-  QueueModel: _QueueModel.default,
-  MongooseQueue: _mongooseQueue.default
-};
-exports.default = _default;
