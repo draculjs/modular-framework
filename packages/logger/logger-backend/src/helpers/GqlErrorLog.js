206f019fb5df7f50d35f04603bf7e32f9b3a2066
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-02 22:41:54 -0300

    Fix GqlErrorLog

--- packages/logger/logger-backend/src/helpers/GqlErrorLog.js
+++ packages/logger/logger-backend/src/helpers/GqlErrorLog.js
@@ -1,17 +1,21 @@
 import GqlErrorLogger from "../loggers/GqlErrorLogger";
 
 function GqlErrorLog(requestContext) {
+    try{
+        if (process.env.LOG_GQL_ERRORS == "ON") {
 
-    if (process.env.LOG_GQL_ERRORS == "ON") {
-
-        let user = requestContext.context.user ? requestContext.context.user.username ? requestContext.context.user.username : "anonymous" : "anonymous"
-        let operation = requestContext.operation ? requestContext.operation.operation : ""
-        requestContext.errors.forEach(error => {
-            let path = error.path[0] || ""
-            let message = `${operation}:${path} by:${user}`
-            GqlErrorLogger.error(message, error)
-        })
+            let user = requestContext.context.user ? requestContext.context.user.username ? requestContext.context.user.username : "anonymous" : "anonymous"
+            let operation = requestContext.operation ? requestContext.operation.operation : ""
+            requestContext.errors.forEach(error => {
+                let path = (error.path && Array.isArray(error.path) && error.path[0]) ? error.path[0] : ""
+                let message = `${operation}:${path} by:${user}`
+                GqlErrorLogger.error(message, error)
+            })
+        }
+    }catch (e){
+        console.error(e)
     }
+
 
 }
 
--- packages/logger/logger-backend/src/helpers/GqlResponseLog.js
+++ packages/logger/logger-backend/src/helpers/GqlResponseLog.js
@@ -5,20 +5,25 @@ const unwrap = (s) => s.replace(
 );
 
 function GqlResponseLog(requestContext) {
-    if (process.env.LOG_GRAPHQL_RESPONSE == "ON") {
+    try {
+        if (process.env.LOG_GRAPHQL_RESPONSE == "ON") {
 
-        let info = {};
-        info.user = requestContext.context.user ? requestContext.context.user.username ? requestContext.context.user.username: "anonymous" : "anonymous"
-        info.type = requestContext.operation ? requestContext.operation.operation ? requestContext.operation.operation.toUpperCase() : "" : ""
-        info.operation = requestContext.operationName || ""
-        info.query = unwrap(requestContext.request.query) || ""
-        info.variables = JSON.stringify(requestContext.request.variables) || ""
-        info.resp = JSON.stringify(requestContext.response.data) || ""
+            let info = {};
+            info.user = requestContext.context.user ? requestContext.context.user.username ? requestContext.context.user.username : "anonymous" : "anonymous"
+            info.type = requestContext.operation ? requestContext.operation.operation ? requestContext.operation.operation.toUpperCase() : "" : ""
+            info.operation = requestContext.operationName || ""
+            info.query = unwrap(requestContext.request.query) || ""
+            info.variables = JSON.stringify(requestContext.request.variables) || ""
+            info.resp = JSON.stringify(requestContext.response.data) || ""
 
-        let message = `GqlResp ${info.type} op:${info.operation} user:${info.user} qry: ${info.query} vars: ${info.variables} resp: ${info.resp} \n`
+            let message = `GqlResp ${info.type} op:${info.operation} user:${info.user} qry: ${info.query} vars: ${info.variables} resp: ${info.resp} \n`
 
-        GqlResponseLogger.info(message)
+            GqlResponseLogger.info(message)
+        }
+    } catch (e) {
+        console.error(e)
     }
+
 }
 
 export default GqlResponseLog
