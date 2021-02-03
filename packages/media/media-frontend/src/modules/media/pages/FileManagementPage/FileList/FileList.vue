28606e990dde0543698ec4db90f94dd435339391
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-03 15:25:58 -0300

    Change headers datatable from data to computed for correct translation

--- packages/media/media-frontend/src/modules/media/pages/FileManagementPage/FileList/FileList.vue
+++ packages/media/media-frontend/src/modules/media/pages/FileManagementPage/FileList/FileList.vue
@@ -1,129 +1,131 @@
 <template>
-    <v-row row wrap>
+  <v-row row wrap>
 
-        <v-col cols="12" sm="6" md="4" offset-md="8" offset-sm="6">
-            <search-input @search="performSearch" v-model="search"/>
-        </v-col>
+    <v-col cols="12" sm="6" md="4" offset-md="8" offset-sm="6">
+      <search-input @search="performSearch" v-model="search"/>
+    </v-col>
 
-        <v-col cols="12">
+    <v-col cols="12">
 
-            <v-data-table
-                    class="mt-3"
-                    :headers="headers"
-                    :items="items"
-                    :search="search"
-                    :single-expand="false"
-                    :server-items-length="totalItems"
-                    :loading="loading"
-                    :page.sync="pageNumber"
-                    :items-per-page.sync="itemsPerPage"
-                    :sort-by.sync="orderBy"
-                    :sort-desc.sync="orderDesc"
-                    :footer-props="{ itemsPerPageOptions: [5, 10, 25, 50] }"
-                    @update:page="fetch"
-                    @update:sort-by="fetch"
-                    @update:sort-desc="fetch"
-                    @update:items-per-page="fetch"
-            >
+      <v-data-table
+          class="mt-3"
+          :headers="headers"
+          :items="items"
+          :search="search"
+          :single-expand="false"
+          :server-items-length="totalItems"
+          :loading="loading"
+          :page.sync="pageNumber"
+          :items-per-page.sync="itemsPerPage"
+          :sort-by.sync="orderBy"
+          :sort-desc.sync="orderDesc"
+          :footer-props="{ itemsPerPageOptions: [5, 10, 25, 50] }"
+          @update:page="fetch"
+          @update:sort-by="fetch"
+          @update:sort-desc="fetch"
+          @update:items-per-page="fetch"
+      >
 
-                <template slot="no-data">
-                    <div class="text-xs-center" v-t="'common.noData'"></div>
-                </template>
+        <template slot="no-data">
+          <div class="text-xs-center" v-t="'common.noData'"></div>
+        </template>
 
-                <template slot="loading">
-                    <div class="text-xs-center" v-t="'common.loading'"></div>
-                </template>
+        <template slot="loading">
+          <div class="text-xs-center" v-t="'common.loading'"></div>
+        </template>
 
-                <template v-slot:item.size="{item}">
-                    {{redeableBytes(item.size)}}
-                </template>
+        <template v-slot:item.size="{item}">
+          {{ redeableBytes(item.size) }}
+        </template>
 
-                <template v-slot:item.createdAt="{item}">
-                    {{formatDate(item.createdAt)}}
-                </template>
+        <template v-slot:item.createdAt="{item}">
+          {{ formatDate(item.createdAt) }}
+        </template>
 
-                <template v-slot:item.action="{ item }">
-                    <show-button @click="$emit('show', item)"/>
-                    <edit-button @click="$emit('update', item)"/>
-                    <delete-button @click="$emit('delete', item)"/>
-                </template>
+        <template v-slot:item.action="{ item }">
+          <show-button @click="$emit('show', item)"/>
+          <edit-button @click="$emit('update', item)"/>
+          <delete-button @click="$emit('delete', item)"/>
+        </template>
 
-            </v-data-table>
-        </v-col>
-    </v-row>
+      </v-data-table>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-    import FileProvider from "../../../providers/FileProvider";
+import FileProvider from "../../../providers/FileProvider";
 
-    import {DeleteButton, EditButton, ShowButton, SearchInput} from "@dracul/common-frontend"
-    import redeableBytesMixin from "../../../mixins/readableBytesMixin";
-    import moment from "moment-timezone"
+import {DeleteButton, EditButton, ShowButton, SearchInput} from "@dracul/common-frontend"
+import redeableBytesMixin from "../../../mixins/readableBytesMixin";
+import moment from "moment-timezone"
 
-    export default {
-        name: "FileList",
-        mixins: [redeableBytesMixin],
-        components: {DeleteButton, EditButton, ShowButton, SearchInput},
-        data() {
-            return {
-                items: [],
-                totalItems: null,
-                loading: false,
-                orderBy: null,
-                orderDesc: false,
-                itemsPerPage: 5,
-                pageNumber: 1,
-                search: '',
-                headers: [
-                    //Entity Headers
-                    {text: this.$t('media.file.filename'), value: 'filename'},
-                    {text: this.$t('media.file.type'), value: 'type'},
-                    {text: this.$t('media.file.size'), value: 'size'},
-                    {text: this.$t('media.file.createdAt'), value: 'createdAt'},
-                    {text: this.$t('media.file.createdBy'), value: 'createdBy.username'},
-                    //Actions
-                    {text: this.$t('common.actions'), value: 'action', sortable: false},
-                ],
-            }
-        },
-        created() {
-            this.fetch()
-        },
-        methods: {
-
-            performSearch() {
-                this.pageNumber = 1
-                this.fetch()
-            },
-            fetch() {
-                this.loading = true
-                FileProvider.paginateFiles(
-                    this.pageNumber,
-                    this.itemsPerPage,
-                    this.search,
-                    this.getOrderBy,
-                    this.getOrderDesc
-                ).then(r => {
-                    this.items = r.data.filePaginate.items
-                    this.totalItems = r.data.filePaginate.totalItems
-                }).catch(err => {
-                    console.error(err)
-                }).finally(() => this.loading = false)
-            }
-        },
-        computed: {
-            formatDate() {
-                return (date) => moment(parseInt(date)).tz("America/Argentina/Buenos_Aires").format('YYYY-MM-DD')
-            },
-            getOrderBy() {
-                return (Array.isArray(this.orderBy)) ? this.orderBy[0] : this.orderBy
-            },
-            getOrderDesc() {
-                return (Array.isArray(this.orderDesc)) ? this.orderDesc[0] : this.orderDesc
-            }
-        }
-
+export default {
+  name: "FileList",
+  mixins: [redeableBytesMixin],
+  components: {DeleteButton, EditButton, ShowButton, SearchInput},
+  data() {
+    return {
+      items: [],
+      totalItems: null,
+      loading: false,
+      orderBy: null,
+      orderDesc: false,
+      itemsPerPage: 5,
+      pageNumber: 1,
+      search: ''
     }
+  },
+  computed: {
+    headers() {
+      return [
+        //Entity Headers
+        {text: this.$t('media.file.filename'), value: 'filename'},
+        {text: this.$t('media.file.type'), value: 'type'},
+        {text: this.$t('media.file.size'), value: 'size'},
+        {text: this.$t('media.file.createdAt'), value: 'createdAt'},
+        {text: this.$t('media.file.createdBy'), value: 'createdBy.username'},
+        //Actions
+        {text: this.$t('common.actions'), value: 'action', sortable: false},
+      ]
+    },
+    formatDate() {
+      return (date) => moment(parseInt(date)).tz("America/Argentina/Buenos_Aires").format('YYYY-MM-DD')
+    },
+    getOrderBy() {
+      return (Array.isArray(this.orderBy)) ? this.orderBy[0] : this.orderBy
+    },
+    getOrderDesc() {
+      return (Array.isArray(this.orderDesc)) ? this.orderDesc[0] : this.orderDesc
+    }
+  },
+  created() {
+    this.fetch()
+  },
+  methods: {
+
+    performSearch() {
+      this.pageNumber = 1
+      this.fetch()
+    },
+    fetch() {
+      this.loading = true
+      FileProvider.paginateFiles(
+          this.pageNumber,
+          this.itemsPerPage,
+          this.search,
+          this.getOrderBy,
+          this.getOrderDesc
+      ).then(r => {
+        this.items = r.data.filePaginate.items
+        this.totalItems = r.data.filePaginate.totalItems
+      }).catch(err => {
+        console.error(err)
+      }).finally(() => this.loading = false)
+    }
+  }
+
+}
 </script>
 
 
--- packages/user/user-frontend/src/pages/GroupManagementPage/GroupList/GroupList.vue
+++ packages/user/user-frontend/src/pages/GroupManagementPage/GroupList/GroupList.vue
@@ -1,139 +1,141 @@
 <template>
-    <v-row row wrap>
+  <v-row row wrap>
 
-        <v-col cols="12" sm="6" md="4" offset-md="8" offset-sm="6">
-            <search-input  @search="performSearch" v-model="search" />
-        </v-col>
+    <v-col cols="12" sm="6" md="4" offset-md="8" offset-sm="6">
+      <search-input @search="performSearch" v-model="search"/>
+    </v-col>
 
-        <v-col cols="12">
-            <v-data-table class="mt-3"
-                          :headers="headers"
-                          :items="items"
-                          :search="search"
-                          :single-expand="false"
-                          :loading="loading"
-                          :server-items-length="totalItems"
-                          :items-per-page.sync="itemsPerPage"
-                          :page.sync="pageNumber"
-                          :sort-by.sync="orderBy"
-                          :sort-desc.sync="orderDesc"
-                          :footer-props="{ itemsPerPageOptions: [5, 10, 25, 50] }"
-                          @update:page="fetch"
-                          @update:items-per-page="fetch"
-                          @update:sort-by="fetch"
-                          @update:sort-desc="fetch"
-            >
+    <v-col cols="12">
+      <v-data-table class="mt-3"
+                    :headers="headers"
+                    :items="items"
+                    :search="search"
+                    :single-expand="false"
+                    :loading="loading"
+                    :server-items-length="totalItems"
+                    :items-per-page.sync="itemsPerPage"
+                    :page.sync="pageNumber"
+                    :sort-by.sync="orderBy"
+                    :sort-desc.sync="orderDesc"
+                    :footer-props="{ itemsPerPageOptions: [5, 10, 25, 50] }"
+                    @update:page="fetch"
+                    @update:items-per-page="fetch"
+                    @update:sort-by="fetch"
+                    @update:sort-desc="fetch"
+      >
 
-                <div slot="no-data" color="info" outline class="text-xs-center">Sin datos</div>
+        <div slot="no-data" color="info" outline class="text-xs-center">Sin datos</div>
 
-                <div slot="loading" outline color="info">Cargando</div>
+        <div slot="loading" outline color="info">Cargando</div>
 
-                <template v-slot:item.avatar="{ item }">
-                    <v-avatar size="36px" :color="item.color?item.color:'grey'">
-                        <span class="white--text headline">{{item.name.charAt(0)}}</span>
-                    </v-avatar>
-                </template>
+        <template v-slot:item.avatar="{ item }">
+          <v-avatar size="36px" :color="item.color?item.color:'grey'">
+            <span class="white--text headline">{{ item.name.charAt(0) }}</span>
+          </v-avatar>
+        </template>
 
-                <template v-slot:item.users="{ item }">
+        <template v-slot:item.users="{ item }">
                     <span>
-                        {{item.users.map(user => user.username).join(", ")}}
+                        {{ item.users.map(user => user.username).join(", ") }}
                     </span>
-                </template>
+        </template>
 
-                <template v-slot:item.action="{ item }">
-                    <v-icon color="info"
-                            small class="mr-2"
-                            @click="$emit('open-show', item)"
-                    >
-                        search
-                    </v-icon>
+        <template v-slot:item.action="{ item }">
+          <v-icon color="info"
+                  small class="mr-2"
+                  @click="$emit('open-show', item)"
+          >
+            search
+          </v-icon>
 
-                    <v-icon color="primary"
-                            small
-                            class="mr-2"
-                            @click="$emit('open-edit', item)"
-                    >
-                        edit
-                    </v-icon>
+          <v-icon color="primary"
+                  small
+                  class="mr-2"
+                  @click="$emit('open-edit', item)"
+          >
+            edit
+          </v-icon>
 
-                    <v-icon color="red"
-                            small
-                            class="mr-2"
-                            @click="$emit('open-delete', item)"
-                    >
-                        delete
-                    </v-icon>
-                </template>
+          <v-icon color="red"
+                  small
+                  class="mr-2"
+                  @click="$emit('open-delete', item)"
+          >
+            delete
+          </v-icon>
+        </template>
 
-            </v-data-table>
-        </v-col>
-    </v-row>
+      </v-data-table>
+    </v-col>
+  </v-row>
 
 
 </template>
 
 <script>
-    import {SearchInput} from '@dracul/common-frontend'
-    import GroupProvider from "../../../providers/GroupProvider";
+import {SearchInput} from '@dracul/common-frontend'
+import GroupProvider from "../../../providers/GroupProvider";
 
-    export default {
-        name: "GroupList",
-        components: {SearchInput},
-        data() {
-            return {
-                items: [],
-                totalItems: null,
-                loading: false,
-                search: '',
-                itemsPerPage: 5,
-                pageNumber: 1,
-                orderBy: null,
-                orderDesc: false,
-                headers: [
-                    {text: this.$t('group.label.avatar'), value: 'avatar'},
-                    {text: this.$t('group.label.name'), value: 'name'},
-                    {text: this.$t('group.label.color'), value: 'color'},
-                    {text: this.$t('group.label.users'), value: 'users'},
-                    {text: this.$t('common.actions'), value: 'action', sortable: false},
-                ],
-            }
-        },
-        computed: {
-            getOrderBy() {
-                return (Array.isArray(this.orderBy)) ? this.orderBy[0] : this.orderBy
-            },
-            getOrderDesc() {
-                return (Array.isArray(this.orderDesc)) ? this.orderDesc[0] : this.orderDesc
-            }
-        },
-        created() {
-            this.fetch()
-        },
-        methods: {
-            performSearch(){
-                this.pageNumber = 1
-                this.fetch()
-            },
-            fetch(){
-
-                this.loading = true
-                GroupProvider.paginateGroups(
-                    this.itemsPerPage,
-                    this.pageNumber,
-                    this.search,
-                    this.getOrderBy,
-                    this.getOrderDesc
-                )
-                    .then(r => {
-                        this.items = r.data.groupsPaginate.items
-                        this.totalItems = r.data.groupsPaginate.totalItems
-                    }).catch(err => {
-                    //TODO improve handle error (show messages to user)
-                    console.error(err)
-                }).finally(() => this.loading = false)
-            }
-        }
+export default {
+  name: "GroupList",
+  components: {SearchInput},
+  data() {
+    return {
+      items: [],
+      totalItems: null,
+      loading: false,
+      search: '',
+      itemsPerPage: 5,
+      pageNumber: 1,
+      orderBy: null,
+      orderDesc: false
     }
+  },
+  computed: {
+    headers() {
+      return [
+        {text: this.$t('group.label.avatar'), value: 'avatar'},
+        {text: this.$t('group.label.name'), value: 'name'},
+        {text: this.$t('group.label.color'), value: 'color'},
+        {text: this.$t('group.label.users'), value: 'users'},
+        {text: this.$t('common.actions'), value: 'action', sortable: false},
+      ]
+    },
+    getOrderBy() {
+      return (Array.isArray(this.orderBy)) ? this.orderBy[0] : this.orderBy
+    },
+    getOrderDesc() {
+      return (Array.isArray(this.orderDesc)) ? this.orderDesc[0] : this.orderDesc
+    }
+  },
+  created() {
+    this.fetch()
+  },
+  methods: {
+    performSearch() {
+      this.pageNumber = 1
+      this.fetch()
+    },
+    fetch() {
+
+      this.loading = true
+      GroupProvider.paginateGroups(
+          this.itemsPerPage,
+          this.pageNumber,
+          this.search,
+          this.getOrderBy,
+          this.getOrderDesc
+      )
+          .then(r => {
+            this.items = r.data.groupsPaginate.items
+            this.totalItems = r.data.groupsPaginate.totalItems
+          }).catch(err => {
+        //TODO improve handle error (show messages to user)
+        console.error(err)
+      }).finally(() => this.loading = false)
+    }
+  }
+}
 </script>
 
 
