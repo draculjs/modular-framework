fdf90530142901cea8a07209870c8e92c5061059
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-02 22:22:33 -0300

    Complete vuetify-messages.js

--- packages/common/common-frontend/src/i18n/messages/vuetify-messages.js
+++ packages/common/common-frontend/src/i18n/messages/vuetify-messages.js
@@ -1,77 +1,215 @@
 const messages = {
     en: {
         $vuetify: {
-            noDataText: "No data",
+            badge: 'Badge',
+            close: 'Close',
             dataIterator: {
-                noResultsText: "No results"
+                noResultsText: 'No matching records found',
+                loadingText: 'Loading items...',
             },
             dataTable: {
-                itemsPerPageText: "Rows per page:",
+                itemsPerPageText: 'Rows per page:',
                 ariaLabel: {
-                    sortNone: "sortNone",
-                    activateAscending: "activateAscending",
-                    activateDescending: "activateDescending",
-                    sortAscending: "sortAscending",
-                    sortDescending: "sortDescending",
-                    activateNone: "activateNone"
-                }
+                    sortDescending: 'Sorted descending.',
+                    sortAscending: 'Sorted ascending.',
+                    sortNone: 'Not sorted.',
+                    activateNone: 'Activate to remove sorting.',
+                    activateDescending: 'Activate to sort descending.',
+                    activateAscending: 'Activate to sort ascending.',
+                },
+                sortBy: 'Sort by',
             },
             dataFooter: {
-                pageText: "{0}-{1} of {2}",
-                nextPage: "next",
-                prevPage: "prev",
-                itemsPerPageAll: 'itemsPerPageAll'
-            }
+                itemsPerPageText: 'Items per page:',
+                itemsPerPageAll: 'All',
+                nextPage: 'Next page',
+                prevPage: 'Previous page',
+                firstPage: 'First page',
+                lastPage: 'Last page',
+                pageText: '{0}-{1} of {2}',
+            },
+            datePicker: {
+                itemsSelected: '{0} selected',
+                nextMonthAriaLabel: 'Next month',
+                nextYearAriaLabel: 'Next year',
+                prevMonthAriaLabel: 'Previous month',
+                prevYearAriaLabel: 'Previous year',
+            },
+            noDataText: 'No data available',
+            carousel: {
+                prev: 'Previous visual',
+                next: 'Next visual',
+                ariaLabel: {
+                    delimiter: 'Carousel slide {0} of {1}',
+                },
+            },
+            calendar: {
+                moreEvents: '{0} more',
+            },
+            fileInput: {
+                counter: '{0} files',
+                counterSize: '{0} files ({1} in total)',
+            },
+            timePicker: {
+                am: 'AM',
+                pm: 'PM',
+            },
+            pagination: {
+                ariaLabel: {
+                    wrapper: 'Pagination Navigation',
+                    next: 'Next page',
+                    previous: 'Previous page',
+                    page: 'Goto Page {0}',
+                    currentPage: 'Current Page, Page {0}',
+                },
+            },
+            rating: {
+                ariaLabel: {
+                    icon: 'Rating {0} of {1}',
+                },
+            },
         }
     },
     es: {
         $vuetify: {
-            noDataText: "Sin Datos",
+            badge: 'Placa',
+            close: 'Cerrar',
             dataIterator: {
-                noResultsText: "Sin Datos"
+                noResultsText: 'Ningún elemento coincide con la búsqueda',
+                loadingText: 'Cargando...',
             },
             dataTable: {
-                itemsPerPageText: "Filas por pagina:",
+                itemsPerPageText: 'Filas por página:',
                 ariaLabel: {
-                    sortNone: "sortNone",
-                    activateAscending: "activateAscending",
-                    activateDescending: "activateDescending",
-                    sortAscending: "sortAscending",
-                    sortDescending: "sortDescending",
-                    activateNone: "activateNone"
-                }
+                    sortDescending: 'Orden descendente.',
+                    sortAscending: 'Orden ascendente.',
+                    sortNone: 'Sin ordenar.',
+                    activateNone: 'Pulse para quitar orden.',
+                    activateDescending: 'Pulse para ordenar descendente.',
+                    activateAscending: 'Pulse para ordenar ascendente.',
+                },
+                sortBy: 'Ordenado por',
             },
             dataFooter: {
-                pageText: "{0}-{1} de {2}",
-                nextPage: "siguiente",
-                prevPage: "anterior",
-                itemsPerPageAll: 'itemsPerPageAll'
+                itemsPerPageText: 'Elementos por página:',
+                itemsPerPageAll: 'Todos',
+                nextPage: 'Página siguiente',
+                prevPage: 'Página anterior',
+                firstPage: 'Primer página',
+                lastPage: 'Última página',
+                pageText: '{0}-{1} de {2}',
+            },
+            datePicker: {
+                itemsSelected: '{0} seleccionados',
+                nextMonthAriaLabel: 'Próximo mes',
+                nextYearAriaLabel: 'Próximo año',
+                prevMonthAriaLabel: 'Mes anterior',
+                prevYearAriaLabel: 'Año anterior',
+            },
+            noDataText: 'No hay datos disponibles',
+            carousel: {
+                prev: 'Visual anterior',
+                next: 'Visual siguiente',
+                ariaLabel: {
+                    delimiter: 'Carousel slide {0} of {1}',
+                },
+            },
+            calendar: {
+                moreEvents: '{0} más',
+            },
+            fileInput: {
+                counter: '{0} archivos',
+                counterSize: '{0} archivos ({1} en total)',
+            },
+            timePicker: {
+                am: 'AM',
+                pm: 'PM',
+            },
+            pagination: {
+                ariaLabel: {
+                    wrapper: 'Navegación de paginación',
+                    next: 'Página siguiente',
+                    previous: 'Página anterior',
+                    page: 'Ir a la página {0}',
+                    currentPage: 'Página actual, página {0}',
+                },
+            },
+            rating: {
+                ariaLabel: {
+                    icon: 'Rating {0} of {1}',
+                },
             }
         }
 
     },
     pt: {
         $vuetify: {
-            noDataText: "Não há dados",
+            badge: 'Distintivo',
+            close: 'Fechar',
             dataIterator: {
-                noResultsText: "Não há dados"
+                noResultsText: 'Nenhum dado encontrado',
+                loadingText: 'Carregando itens...',
             },
             dataTable: {
-                itemsPerPageText: "Linhas por página",
+                itemsPerPageText: 'Linhas por página:',
                 ariaLabel: {
-                    sortNone: "sortNone",
-                    activateAscending: "activateAscending",
-                    activateDescending: "activateDescending",
-                    sortAscending: "sortAscending",
-                    sortDescending: "sortDescending",
-                    activateNone: "activateNone"
-                }
+                    sortDescending: 'Ordenado decrescente.',
+                    sortAscending: 'Ordenado crescente.',
+                    sortNone: 'Não ordenado.',
+                    activateNone: 'Ative para remover a ordenação.',
+                    activateDescending: 'Ative para ordenar decrescente.',
+                    activateAscending: 'Ative para ordenar crescente.',
+                },
+                sortBy: 'Ordenar por',
             },
             dataFooter: {
-                pageText: "page",
-                nextPage: "next",
-                prevPage: "prev",
-                itemsPerPageAll: 'itemsPerPageAll'
+                itemsPerPageText: 'Itens por página:',
+                itemsPerPageAll: 'Todos',
+                nextPage: 'Próxima página',
+                prevPage: 'Página anterior',
+                firstPage: 'Primeira página',
+                lastPage: 'Última página',
+                pageText: '{0}-{1} de {2}',
+            },
+            datePicker: {
+                itemsSelected: '{0} selecionado(s)',
+                nextMonthAriaLabel: 'Próximo mês',
+                nextYearAriaLabel: 'Próximo ano',
+                prevMonthAriaLabel: 'Mês anterior',
+                prevYearAriaLabel: 'Ano anterior',
+            },
+            noDataText: 'Não há dados disponíveis',
+            carousel: {
+                prev: 'Visão anterior',
+                next: 'Próxima visão',
+                ariaLabel: {
+                    delimiter: 'Slide {0} de {1} do carrossel',
+                },
+            },
+            calendar: {
+                moreEvents: 'Mais {0}',
+            },
+            fileInput: {
+                counter: '{0} arquivo(s)',
+                counterSize: '{0} arquivo(s) ({1} no total)',
+            },
+            timePicker: {
+                am: 'AM',
+                pm: 'PM',
+            },
+            pagination: {
+                ariaLabel: {
+                    wrapper: 'Navegação de paginação',
+                    next: 'Próxima página',
+                    previous: 'Página anterior',
+                    page: 'Ir à página {0}',
+                    currentPage: 'Página atual, página {0}',
+                },
+            },
+            rating: {
+                ariaLabel: {
+                    icon: 'Rating {0} of {1}',
+                },
             }
         }
 
