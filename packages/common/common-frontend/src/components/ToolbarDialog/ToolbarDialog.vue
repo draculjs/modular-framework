21ebe53955dad1a83abf5760451cecf29ac0111f
Cristian Incarnato <cristian.cdi@gmail.com>
2021-03-22 22:10:29 -0300

    Fix ToolbarDialog.vue

--- packages/common/common-frontend/src/components/ToolbarDialog/ToolbarDialog.vue
+++ packages/common/common-frontend/src/components/ToolbarDialog/ToolbarDialog.vue
@@ -3,7 +3,7 @@
         <v-toolbar-title :class="danger?'white--text':'onPrimary--text'" v-t="title"></v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn icon :class="danger?'white--text':'onPrimary--text'" @click="$emit('close')">
+            <v-btn icon :class="danger?'white--text':'primary onPrimary--text'" @click="$emit('close')">
                 <v-icon>close</v-icon>
             </v-btn>
         </v-toolbar-items>
