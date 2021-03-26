dd8fb328bcc867596132094e27ea827a71d037b9
Cristian Incarnato <cristian.cdi@gmail.com>
2021-03-25 21:59:25 -0300

    Fix export

--- packages/common/common-backend/src/index.js
+++ packages/common/common-backend/src/index.js
@@ -2,11 +2,11 @@ const storeFile = require('./helpers/storeFile')
 const randomString = require('./helpers/randomString')
 const randomNumber = require('./helpers/randomNumber')
 const createDirIfNotExist = require('./helpers/createDirIfNotExist')
-import {types as commonTypes} from './graphql'
+const {types} =  require('./graphql')
 
 module.exports.storeFile = storeFile
 module.exports.randomString = randomString
 module.exports.randomNumber = randomNumber
 module.exports.createDirIfNotExist = createDirIfNotExist
-module.exports.commonTypes = commonTypes
+module.exports.commonTypes = types
 
