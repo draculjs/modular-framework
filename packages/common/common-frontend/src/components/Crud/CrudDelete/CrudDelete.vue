cdc72489b74ce6b0cc23c55ff2498031fd72690f
Cristian Incarnato <cristian.cdi@gmail.com>
2021-03-29 10:28:02 -0300

    add fullscreen option to dialogs

--- packages/common/common-frontend/src/components/Crud/CrudDelete/CrudDelete.vue
+++ packages/common/common-frontend/src/components/Crud/CrudDelete/CrudDelete.vue
@@ -1,44 +1,45 @@
 <template>
-    <v-dialog :value="open" max-width="850" persistent >
-        <v-card>
+  <v-dialog :value="open" max-width="850" persistent :fullscreen="fullscreen">
+    <v-card>
 
-            <toolbar-dialog
-                    danger
-                    :title="title"
-                    @close="$emit('close')"
-            />
+      <toolbar-dialog
+          danger
+          :title="title"
+          @close="$emit('close')"
+      />
 
-            <v-card-text>
-                <slot></slot>
-            </v-card-text>
+      <v-card-text>
+        <slot></slot>
+      </v-card-text>
 
-            <v-divider></v-divider>
+      <v-divider></v-divider>
 
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <close-button color="grey darken-2" @click="$emit('close')"></close-button>
-                <submit-button :loading="loading" danger @click="$emit('delete')" text="common.delete" ></submit-button>
-            </v-card-actions>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <close-button color="grey darken-2" @click="$emit('close')"></close-button>
+        <submit-button :loading="loading" danger @click="$emit('delete')" text="common.delete"></submit-button>
+      </v-card-actions>
 
 
-        </v-card>
-    </v-dialog>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-    import ToolbarDialog from "../../ToolbarDialog";
-    import CloseButton from "../../CloseButton/CloseButton";
-    import SubmitButton from "../../SubmitButton/SubmitButton";
+import ToolbarDialog from "../../ToolbarDialog";
+import CloseButton from "../../CloseButton/CloseButton";
+import SubmitButton from "../../SubmitButton/SubmitButton";
 
-    export default {
-        name: "CrudDelete",
-        components: {SubmitButton, CloseButton, ToolbarDialog},
-        props: {
-            title: {type: String, default: 'common.delete'},
-            open: {type: Boolean, default: false},
-            loading: {type: Boolean, default: false}
-        }
-    }
+export default {
+  name: "CrudDelete",
+  components: {SubmitButton, CloseButton, ToolbarDialog},
+  props: {
+    title: {type: String, default: 'common.delete'},
+    open: {type: Boolean, default: false},
+    loading: {type: Boolean, default: false},
+    fullscreen: {type: Boolean, default: false}
+  }
+}
 </script>
 
 <style scoped>
