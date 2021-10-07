e81a29045110b1d629280733fcdf78256e28a7f3
Cristian Incarnato <cristian.cdi@gmail.com>
2021-10-07 09:07:59 -0300

    Fix showCurrentPassword

--- packages/queue/mongoose-queue/lib/services/QueueService.js
+++ packages/queue/mongoose-queue/lib/services/QueueService.js
@@ -61,9 +61,15 @@ const getJob = function (topic, workerId, maxRetries, blockDuration) {
       blockedUntil: {
         $lt: Date.now()
       },
-      $expr: {
-        $lt: ["$retries", "$maxRetries"]
-      },
+      //Fix MongoDB 3.6+
+      ...(!process.env.MONGO_OLD && {
+        $expr: {
+          $lt: ["$retries", "$maxRetries"]
+        }
+      }),
+      ...(process.env.MONGO_OLD && {
+        "$where": "this.retries > this.maxRetries"
+      }),
       done: false
     }, {
       $set: {
