1f182cfa198e7109fbb387624c2da07cbba8842e
Juanca <juancarabellino@gmail.com>
2021-10-07 11:54:31 -0300

    Corrijo ambito de la variable

--- packages/media/media-backend/src/modules/media/services/UploadService.js
+++ packages/media/media-backend/src/modules/media/services/UploadService.js
@@ -4,72 +4,72 @@ import storeFile from './helpers/storeFile'
 import randomString from './helpers/randomString'
 
 const baseUrl = function () {
-    let url = process.env.APP_API_URL
+  let url = process.env.APP_API_URL
 
-    if (!/^http:\/\//.test(url)) {
-        url = "http://" + baseUrl
-    }
-
-    if (!/\/$/.test(url)) {
-        url += "/"
-    }
-    
-    return url
+  if (!/^http:\/\//.test(url)) {
+    url = "http://" + baseUrl
   }
-  
-  const fileUpload = function (user, inputFile) {
-    
-    return new Promise(async (resolve, rejects) => {
-      try {
 
-        const {filename, mimetype, encoding, createReadStream} = await inputFile;
+  if (!/\/$/.test(url)) {
+    url += "/"
+  }
 
-        let type = mimetype.split("/")[0]
+  return url
+}
 
-        const parseFileName = path.parse(filename);
-        const extension = parseFileName.ext
-        const name = parseFileName.name
-        const hash = '-' + randomString(6)
-        const finalFileName = name + hash + extension
-        const year = new Date().getFullYear().toString()
-        const month = (new Date().getMonth() + 1).toString()
-        const relativePath = path.join("media", "files", user.username, year,month, finalFileName)
-        const absolutePath = path.resolve(relativePath);
+const fileUpload = function (user, inputFile) {
 
-        //Store
-        var storeResult = await storeFile(createReadStream(), relativePath)
+  return new Promise(async (resolve, rejects) => {
+    try {
 
-        let url = baseUrl() + relativePath
-        
-        if (storeResult && storeResult.finish) {
+      const { filename, mimetype, encoding, createReadStream } = await inputFile;
 
-            file.create({
-                filename: finalFileName,
-                mimetype: mimetype,
-                encoding: encoding,
-                type: type,
-                extension: extension,
-                relativePath: relativePath,
-                absolutePath: absolutePath,
-                size: storeResult.bytesWritten,
-                url: url,
-                createdBy: {user: user.id, username: user.username}
-            }, function (err, doc) {
-                if (err) return rejects(err);
-                // saved!
-                doc.populate('createdBy.user').execPopulate(() => (resolve(doc)))
-            });
-            
-        } else {
-            rejects(new Error("Upload Fail"))
-        }
-          
-        } catch (error) {
-            rejects(new Error("Upload Fail"))
-        }
-    })
+      let type = mimetype.split("/")[0]
+
+      const parseFileName = path.parse(filename);
+      const extension = parseFileName.ext
+      const name = parseFileName.name
+      const hash = '-' + randomString(6)
+      const finalFileName = name + hash + extension
+      const year = new Date().getFullYear().toString()
+      const month = (new Date().getMonth() + 1).toString()
+      const relativePath = path.join("media", "files", user.username, year, month, finalFileName)
+      const absolutePath = path.resolve(relativePath);
+
+      //Store
+      let storeResult = await storeFile(createReadStream(), relativePath)
+
+      let url = baseUrl() + relativePath
+
+      if (storeResult && storeResult.finish) {
+
+        file.create({
+          filename: finalFileName,
+          mimetype: mimetype,
+          encoding: encoding,
+          type: type,
+          extension: extension,
+          relativePath: relativePath,
+          absolutePath: absolutePath,
+          size: storeResult.bytesWritten,
+          url: url,
+          createdBy: { user: user.id, username: user.username }
+        }, function (err, doc) {
+          if (err) return rejects(err);
+          // saved!
+          doc.populate('createdBy.user').execPopulate(() => (resolve(doc)))
+        });
+
+      } else {
+        rejects(new Error("Upload Fail"))
+      }
+
+    } catch (error) {
+      rejects(new Error("Upload Fail"))
+    }
+  })
 
 }
 
-export {fileUpload}
+export { fileUpload }
 export default fileUpload
