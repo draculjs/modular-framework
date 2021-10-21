54189f8928dd74c582612c6d4573ad524efffe0c
lrojas <lujan.rojas@sondeos.com.ar>
2021-10-21 08:52:10 -0300

    agrego vista con documentacion de media y links utiles

--- packages/media/media-frontend/src/modules/base/pages/AboutPage/AboutPage.vue
+++ packages/media/media-frontend/src/modules/base/pages/AboutPage/AboutPage.vue
@@ -2,15 +2,27 @@
     <v-container fill-height>
         <v-row justify="center" align="center">
             <v-col cols="12" class="text-center">
-                <v-icon>home</v-icon>
+              <h3 class="mb-2">Dracul Media - Documentacion</h3>
+              <about-list :items="dataList"/>
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
+    import AboutList from "../../components/AboutList";
     export default {
         name: 'AboutPage',
+      components: {AboutList},
+      data(){return{
+        dataList: [{title: 'Acerca de Dracul Media', subtitle: 'Informacion acerca de como levantar el proyecto en local.', link: 'https://github.com/draculjs/modular-framework/tree/master/packages/media'},
+          {title: 'Docker', subtitle: 'Informacion acerca de como levantar el proyecto con Docker.', link: 'https://github.com/draculjs/modular-framework/blob/master/packages/media/readme-docker.md'},
+          {title: 'Documentacion frontend', subtitle: 'Listado de rutas, vistas y descripcion de componentes.', link: 'https://github.com/draculjs/modular-framework/blob/master/packages/media/media-frontend/docs-es.md'},
+          {title:'Documentacion backend', subtitle: 'Endpoints de GraphQL y REST. Descripcion de los servicios que provee.', link: 'https://github.com/draculjs/modular-framework/blob/master/packages/media/media-backend/docs-es.md'},
+          {title:'Invocaciones con GraphQL', subtitle: 'Se sugiere usar la extensión "Altair Graphql cliente" la cual permite subir archivos).', link: 'https://github.com/draculjs/modular-framework/blob/master/packages/media/media-backend/docs-es.md#queries'},
+          {title: 'Swagger', subtitle: 'Link a la documentacion de endpoints REST con Swagger.',link: process.env.VUE_APP_APIHOST+'/api-docs'},
+          {title: 'Repositorios con ejemplos', subtitle: 'Ejemplos con diferentes lenguajes (PHP, Javascript, Java). (En desarrollo).',link: ''}]
+      }}
     }
 </script>
 
