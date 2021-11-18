84937681e9c798b0b222597832280779fc40474c
Juanca <juancarabellino@gmail.com>
2021-11-18 11:04:13 -0300

    Nueva versión dracul media

--- packages/media/media-backend/lib/mongo-db.js
+++ packages/media/media-backend/lib/mongo-db.js
@@ -1,23 +1,60 @@
 "use strict";
 
-var _dotenv = _interopRequireDefault(require("dotenv"));
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_dotenv.default.config();
-
-_mongoose.default.Promise = global.Promise;
-
-_mongoose.default.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true
-});
-
 const {
-  ObjectId
-} = _mongoose.default.Types;
+  DefaultLogger
+} = require('@dracul/logger-backend');
 
-ObjectId.prototype.valueOf = function () {
-  return this.toString();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+const mongoConnect = async function () {
+  if (!process.env.MONGO_URI) {
+    DefaultLogger.error("MongoDB connection error: process.env.MONGO_URI not found");
+    throw new Error("process.env.MONGO_URI not found");
+  }
+
+  try {
+    await connectToMongo(process.env.MONGO_URI);
+    const db = mongoose.connection;
+    db.on('error', function () {
+      console.error.bind(console, 'connection error:');
+      DefaultLogger.info("Reconnecting with MongoDB");
+      setTimeout(() => connectToMongo(process.env.MONGO_URI), 3000);
+    });
+    db.once('open', function () {
+      DefaultLogger.info("MongoDB Open");
+    });
+    const {
+      ObjectId
+    } = mongoose.Types;
+
+    ObjectId.prototype.valueOf = function () {
+      return this.toString();
+    };
+  } catch (error) {
+    DefaultLogger.error("Connection to Mongo error: " + error);
+    throw new Error("Connection to Mongo error: " + error);
+  }
 };
+
+const connectToMongo = function (mongoUri) {
+  return new Promise((resolve, reject) => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }).then(() => {
+      DefaultLogger.info("Mongoose connected");
+      resolve();
+    }).catch(error => {
+      DefaultLogger.error("Mongoose not connected", error);
+      reject(error);
+    });
+  });
+};
+
+module.exports = mongoConnect;
