9b5134d7c724192db94fc552feea137e9869a0f6
Cristian Incarnato <cristian.cdi@gmail.com>
2021-12-29 13:02:20 -0300

    Improve UserStorageForm

--- packages/media/media-backend/lib/modules/media/graphql/resolvers/UserStorageResolver.js
+++ packages/media/media-backend/lib/modules/media/graphql/resolvers/UserStorageResolver.js
@@ -28,6 +28,12 @@ var _default = {
       if (!user) throw new _apolloServerExpress.AuthenticationError("Unauthenticated");
       if (!rbac.isAllowed(user.id, _UserStorage.USER_STORAGE_SHOW_OWN)) throw new _apolloServerExpress.ForbiddenError("Not Authorized");
       return (0, _UserStorageService.findUserStorageByUser)(user);
+    },
+    fetchMediaVariables: (_, {}) => {
+      return Promise.resolve({
+        maxFileSize: process.env.MEDIA_MAX_SIZE_PER_FILE_IN_MEGABYTES || 1024,
+        fileExpirationTime: process.env.MEDIA_FILE_EXPIRATION_TIME_IN_DAYS || 365
+      });
     }
   },
   Mutation: {
