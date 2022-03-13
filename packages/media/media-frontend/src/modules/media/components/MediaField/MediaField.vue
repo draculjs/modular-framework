d0b4425db25027dc6660c9d31fc4db5ade34c651
Cristian Incarnato <cristian.cdi@gmail.com>
2022-03-13 19:13:40 -0300

    fix media field

--- packages/media/media-frontend/src/modules/media/components/MediaField/MediaField.vue
+++ packages/media/media-frontend/src/modules/media/components/MediaField/MediaField.vue
@@ -26,7 +26,7 @@
 
 <script>
     import {InputErrorsByProps, RequiredRule} from "@dracul/common-frontend";
-    import {FileUploadExpress} from "../FileUploadExpiration";
+    import {FileUploadExpress} from "../FileUploadExpress";
 
     export default {
         name: "MediaField",
