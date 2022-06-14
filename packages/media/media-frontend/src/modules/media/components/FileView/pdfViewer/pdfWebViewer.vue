59cd14d3af418328ee5fb5e7df7b58c6a2f4bfa8
ramirosandoval <ramiro_sando@outlook.com>
2022-06-14 01:32:11 -0300

    web viewer component's style corrected

--- packages/media/media-frontend/src/modules/media/components/FileView/pdfViewer/pdfWebViewer.vue
+++ packages/media/media-frontend/src/modules/media/components/FileView/pdfViewer/pdfWebViewer.vue
@@ -3,32 +3,28 @@
 </template>
 
 <script>
-import WebViewer from '@pdftron/pdfjs-express'
-export default {
-  name: 'WebViewer',
-  props: {
-    url: String
-  },
-  mounted: function () {
-        WebViewer({
-            licenseKey: 't8aqS5zBEnL0qpxr5pWj',
-            path: '/public',
-            initialDoc: this.url,
-          }, this.$refs.viewer).then((instance) => {
-            instance.disableFeatures([instance.Feature.Print, instance.Feature.Copy, instance.Feature.Download]);
-          });
+    import WebViewer from '@pdftron/pdfjs-express'
+    export default {
+        name: 'WebViewer',
+        props: {
+            url: String,
+        },
+
+        mounted: function () {
+            WebViewer({
+                licenseKey: 't8aqS5zBEnL0qpxr5pWj',
+                path: '/public',
+                initialDoc: this.url,
+            }, this.$refs.viewer).then((instance) => {
+                instance.disableFeatures([instance.Feature.Print, instance.Feature.Copy, instance.Feature.Download]);
+            });
+        }
     }
-}
-  document.addEventListener('keyup', (e) => {
-    if (e.key == 'PrintScreen') {
-      navigator.clipboard.writeText('');
-    }
-  });
 </script>
 
 <style scoped>
 div {
-  width: 375px; 
-  height: 300px;
+  width: 100%; 
+  height: 100%;
 }
 </style>
