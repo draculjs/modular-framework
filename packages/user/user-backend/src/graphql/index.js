60ada0ae8a41031224b3e5bfd76c799d251d82cf
Cristian Incarnato <cristian.cdi@gmail.com>
2022-07-04 21:00:37 -0300

    Fix security types

--- packages/user/user-backend/src/graphql/index.js
+++ packages/user/user-backend/src/graphql/index.js
@@ -4,9 +4,9 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 
 //TYPES
 const typesArray = loadFilesSync(path.join(__dirname, './types'));
-export const types =  mergeTypeDefs(typesArray, { all: true });
+export const securityTypes =  mergeTypeDefs(typesArray, { all: true });
 
 
 //RESOLVERS
 const resolversArray = loadFilesSync(path.join(__dirname, './resolvers'));
-export const resolvers =  mergeResolvers(resolversArray);
+export const securityResolvers =  mergeResolvers(resolversArray);
