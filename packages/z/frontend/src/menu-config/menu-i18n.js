481e81c94d9207397814480cc82293bed6f36d7d
ramirosandoval <ramiro_sando@outlook.com>
2022-08-31 12:17:03 -0300

    Se añade boton para agregar settings al home de package z

--- packages/z/frontend/src/menu-config/menu-i18n.js
+++ packages/z/frontend/src/menu-config/menu-i18n.js
@@ -4,21 +4,25 @@ const messages = {
         menu: {
             userdashboard: 'Users Dashboard',
             customization: 'Customization',
-            administration: 'Administration'
+            administration: 'Administration',
+            settings: 'Ajustes',
         }
     },
     es: {
         menu: {
             userdashboard: 'Tablero de Usuarios',
             customization: 'Personalización',
-            administration: 'Administración'
+            administration: 'Administración',
+            settings: 'Ajustes',
+
         }
     },
     pt: {
         menu: {
             userdashboard: 'Painel de controle',
             customization: 'Costumização',
-            administration: 'administração'
+            administration: 'administração',
+            settings: 'Ajustes'
         }
     }
 }
