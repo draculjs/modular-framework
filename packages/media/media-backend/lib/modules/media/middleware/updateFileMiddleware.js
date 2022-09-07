f04630e2412ed53b8d1779d946c6fd0a58f3debd
Carolina Romero <carolina.romero@sondeos.com.ar>
2022-09-07 10:19:06 -0300

    Contador de visualizaciones refactor (hits)

--- packages/media/media-backend/lib/modules/media/middleware/updateFileMiddleware.js
+++ packages/media/media-backend/lib/modules/media/middleware/updateFileMiddleware.js
@@ -11,7 +11,11 @@ function _default(req, res, next) {
   try {
     // Elimino la primera barra para poder comparar contra el campo 'relativePath' del modelo
     let uri_dec = decodeURIComponent(req.originalUrl).replace('/', '');
-    (0, _FileService.updateByRelativePath)(uri_dec);
+
+    if (!req.headers.toString().includes("Content-Range")) {
+      (0, _FileService.updateByRelativePath)(uri_dec);
+    }
+
     next();
   } catch (error) {
     console.error("updateFileMiddleware error:", error);
