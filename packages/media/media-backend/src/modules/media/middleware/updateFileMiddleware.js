3fa45f79ea4ac83aad3074fee0a3f3f708c0adc8
Carolina Romero <carolina.romero@sondeos.com.ar>
2022-09-14 10:29:29 -0300

    Dependencias actualizadas

--- packages/media/media-backend/src/modules/media/middleware/updateFileMiddleware.js
+++ packages/media/media-backend/src/modules/media/middleware/updateFileMiddleware.js
@@ -4,7 +4,6 @@ import { updateByRelativePath } from "../services/FileService"
 export default function (req, res, next) {
     try {
 
-        console.log("ARCHIVO: ",!req.headers.toString().includes("Content-Range"))
         // Elimino la primera barra para poder comparar contra el campo 'relativePath' del modelo
         let uri_dec = decodeURIComponent(req.originalUrl).replace('/', '');
         if(!req.headers.toString().includes("Content-Range")){
