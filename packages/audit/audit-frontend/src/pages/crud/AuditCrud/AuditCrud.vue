8b0789b9c7994fee84c8810cf0ea228ed7578553
ramirosandoval <ramiro_sando@outlook.com>
2022-10-14 14:08:36 -0300

    i18n translation usage on AuditCrud fixed

--- packages/audit/audit-frontend/src/pages/crud/AuditCrud/AuditCrud.vue
+++ packages/audit/audit-frontend/src/pages/crud/AuditCrud/AuditCrud.vue
@@ -1,116 +1,115 @@
 <template>
-<crud-layout :title="title" :subtitle="subtitle">
+    <crud-layout :title="title" :subtitle="subtitle">
 
         <template v-slot:list>
             <audit-list 
-                       ref="list"
-                       @update="update"
-                       @delete="remove"
-                       @show="show"
-            
+                ref="list"
+                @update="update"
+                @delete="remove"
+                @show="show"
             />
         </template>
-        
-         <add-button v-if="$store.getters.hasPermission('AUDIT_CREATE')" @click="create"></add-button>
-      
+
+        <add-button v-if="$store.getters.hasPermission('AUDIT_CREATE')" @click="create"></add-button>
+
         <audit-create v-if="creating" 
-                        :open="creating"
-                        v-on:itemCreated="onItemCreated" 
-                        v-on:close="creating=false" 
+            :open="creating"
+            v-on:itemCreated="onItemCreated" 
+            v-on:close="creating=false" 
         />
-        
+
         <audit-update v-if="updating" 
-                        :open="updating"
-                        :item="itemToEdit" 
-                        v-on:itemUpdated="onItemUpdated" 
-                        v-on:close="updating=false" 
+            :open="updating"
+            :item="itemToEdit" 
+            v-on:itemUpdated="onItemUpdated" 
+            v-on:close="updating=false" 
         />
-          
+
         <audit-show v-if="showing" 
-                           :open="showing" 
-                           :item="itemToShow"  
-                           v-on:close="showing=false" 
+            :open="showing" 
+            :item="itemToShow"  
+            v-on:close="showing=false" 
          />
 
         <audit-delete v-if="deleting" 
-                         :open="deleting"
-                         :item="itemToDelete"  
-                         v-on:itemDeleted="onItemDeleted" 
-                         v-on:close="deleting=false" 
+            :open="deleting"
+            :item="itemToDelete"  
+            v-on:itemDeleted="onItemDeleted" 
+            v-on:close="deleting=false" 
         />
 
-        <snackbar v-model="flash"/>
+        <snackbar v-model="flash" />
 
-</crud-layout>
+    </crud-layout>
 </template>
 
 <script>
-    
-    import AuditCreate from "../AuditCreate";
-    import AuditUpdate from "../AuditUpdate";
-    import AuditDelete from "../AuditDelete";
-    import AuditShow from "../AuditShow";
-    import AuditList from "../AuditList";
-    
-     import {CrudLayout, AddButton, Snackbar} from "@dracul/common-frontend"
-     
-    export default {
-        name: "AuditCrud",
-        components: {
-            CrudLayout, AddButton, Snackbar,
-            AuditCreate, 
-            AuditUpdate, 
-            AuditDelete, 
-            AuditShow,
-            AuditList
-        },
-        data() {
-            return {
-                title: 'audit.title',
-                subtitle: 'audit.subtitle',
-                flash: null,
-                creating: false,
-                updating: false,
-                deleting: false,
-                showing: false,
-                itemToEdit: null,
-                itemToDelete: null,
-                itemToShow: null,
-            }
-        },
-        methods: {
-            //On
-            onItemCreated() {
-                this.$refs.list.fetch()
-                this.flash= "common.created"
-            },
-            onItemUpdated() {
-                this.$refs.list.fetch()
-                this.flash= "common.updated"
-            },
-            onItemDeleted() {
-                this.$refs.list.fetch()
-                this.flash= "common.deleted"
-            },
-            //Open
-            create() {
-                this.creating = true
-            },
-            update(item) {
-                this.updating = true
-                this.itemToEdit = item
-            },
-            show(item) {
-                this.showing = true
-                this.itemToShow = item
-            },
-            remove(item) {
-                this.deleting = true
-                this.itemToDelete = item
-            }
+
+import AuditCreate from "../AuditCreate";
+import AuditUpdate from "../AuditUpdate";
+import AuditDelete from "../AuditDelete";
+import AuditShow from "../AuditShow";
+import AuditList from "../AuditList";
+
+import { CrudLayout, AddButton, Snackbar } from "@dracul/common-frontend"
+
+export default {
+    name: "AuditCrud",
+    components: {
+        CrudLayout, AddButton, Snackbar,
+        AuditCreate,
+        AuditUpdate,
+        AuditDelete,
+        AuditShow,
+        AuditList
+    },
+    data() {
+        return {
+            title: 'audit.title',
+            subtitle: 'audit.subtitle',
+            flash: null,
+            creating: false,
+            updating: false,
+            deleting: false,
+            showing: false,
+            itemToEdit: null,
+            itemToDelete: null,
+            itemToShow: null,
         }
-        
+    },
+    methods: {
+        //On
+        onItemCreated() {
+            this.$refs.list.fetch()
+            this.flash = this.$t('common.created')
+        },
+        onItemUpdated() {
+            this.$refs.list.fetch()
+            this.flash = this.$t('common.updated')
+        },
+        onItemDeleted() {
+            this.$refs.list.fetch()
+            this.flash = this.$t('common.deleted')
+        },
+        //Open
+        create() {
+            this.creating = true
+        },
+        update(item) {
+            this.updating = true
+            this.itemToEdit = item
+        },
+        show(item) {
+            this.showing = true
+            this.itemToShow = item
+        },
+        remove(item) {
+            this.deleting = true
+            this.itemToDelete = item
+        }
     }
+
+}
 </script>
 
 
