db7bb232e008f16dcf854774d5751dd0609b92a3
ramirosandoval <ramiro_sando@outlook.com>
2022-10-17 15:17:53 -0300

    AuditProvider is now exported from AuditFrontend

--- packages/audit/audit-frontend/src/index.js
+++ packages/audit/audit-frontend/src/index.js
@@ -1,9 +1,11 @@
-import Audit from "./pages/crud/index.vue";
+import Audit from "./pages/crud/index.vue"
 import routes from "../routes/"
+import AuditProvider from "../providers/AuditProvider"
 import { i18nMessages } from "../i18n/messages/audit-messages.js"
 
 export {
     Audit,
     routes,
-    i18nMessages
+    i18nMessages,
+    AuditProvider
 }
