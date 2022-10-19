deb55fdf0c62575718d6918e54bd51e8ac174c2a
ramirosandoval <ramiro_sando@outlook.com>
2022-10-19 16:37:44 -0300

    Updated audit-backend to export Audit Services

--- packages/audit/audit-backend/src/index.js
+++ packages/audit/audit-backend/src/index.js
@@ -1,14 +1,19 @@
-import {types, resolvers} from "../graphql/"
+import { types, resolvers } from "../graphql/"
 import * as permissions from "../permissions/AuditPermissions"
 import AuditModel from "../models/AuditModel"
+import { createAudit, updateAudit, fetchAudit, deleteAudit } from "../services/AuditService.js"
 
-export{
+export {
     //Services
+    createAudit,
+    updateAudit,
+    fetchAudit,
+    deleteAudit,
     //Types and resolvers
     types,
     resolvers,
     //Permissions
     permissions,
     //Model
-    AuditModel
+    AuditModel,
 }
