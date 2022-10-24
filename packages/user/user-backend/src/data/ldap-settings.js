7f76265571dff75bc9db34e84342fac6d046a5a0
bloome-alex <alex99valdiviezo@gmail.com>
2022-10-24 17:20:36 -0300

    delete rejects from ldap-settings

--- packages/user/user-backend/src/data/ldap-settings.js
+++ packages/user/user-backend/src/data/ldap-settings.js
@@ -3,29 +3,25 @@ export const LDAP_SETTINGS = [
         key: 'LDAP_AUTH',
         type: 'boolean',
         value: false,
-        label: {en: 'LDAP AUTH', es: 'LDAP AUTH', pt: 'LDAP AUTH'},
-        reject: 'none'
+        label: {en: 'LDAP AUTH', es: 'LDAP AUTH', pt: 'LDAP AUTH'}
     },
     {
         key: 'LDAP_DEFAULT_ROLE',
         type: 'string',
         value: '',
-        label: {en: 'LDAP DEFAULT ROLE', es: 'LDAP DEFAULT ROLE', pt: 'LDAP DEFAULT ROLE'},
-        reject: 'none'
+        label: {en: 'LDAP DEFAULT ROLE', es: 'LDAP DEFAULT ROLE', pt: 'LDAP DEFAULT ROLE'}
     },
     {
         key: 'LDAP_DN',
         type: 'string',
         value: '',
-        label: {en: 'LDAP DN', es: 'LDAP DN', pt: 'LDAP DN'},
-        reject: 'none'
+        label: {en: 'LDAP DN', es: 'LDAP DN', pt: 'LDAP DN'}
     },
     {
         key: 'LDAP_OU',
         type: 'string',
         value: '',
-        label: {en: 'LDAP OU', es: 'LDAP OU', pt: 'LDAP OU'},
-        reject: 'none'
+        label: {en: 'LDAP OU', es: 'LDAP OU', pt: 'LDAP OU'}
     },
     {
         key: 'LDAP_IP',
