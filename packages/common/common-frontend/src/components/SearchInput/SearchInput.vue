40df1775bb3aa801a95df0e14d5766788b687c05
Cristian Incarnato <cristian.cdi@gmail.com>
2023-01-27 18:20:51 -0300

    SearchInput clearable

--- packages/common/common-frontend/src/components/SearchInput/SearchInput.vue
+++ packages/common/common-frontend/src/components/SearchInput/SearchInput.vue
@@ -1,32 +1,43 @@
 <template>
-    <v-text-field
-            v-model="search"
-            append-icon="search"
-            :label="$t(label)"
-            single-line
-            hide-details
-            v-on:keyup.native.enter="$emit('search')"
-    ></v-text-field>
+  <v-text-field
+      v-model="search"
+      :prepend-inner-icon="prependInnerIcon"
+      :label="$t(label)"
+      single-line
+      hide-details
+      v-on:keyup.native.enter="$emit('search')"
+      :clearable="clearable" @click:clear="onClear"
+  ></v-text-field>
 </template>
 
 <script>
-    export default {
-        name: "SearchInput",
-        props: {
-            value: {type:String, default: ''},
-            label: {type:String, default: 'common.search'}
-        },
-        computed: {
-            search: {
-                get(){
-                    return this.value
-                },
-                set(val){
-                    this.$emit('input',val)
-                }
-            }
-        }
+export default {
+  name: "SearchInput",
+  props: {
+    value: {type: String, default: ''},
+    label: {type: String, default: 'common.search'},
+    clearable: {type: Boolean, default: true},
+    prependInnerIcon: {type: String, default: 'search'},
+
+  },
+  computed: {
+    search: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
     }
+  },
+  methods:{
+    onClear(){
+      setTimeout(() => {
+        this.$emit("search")
+      },10)
+    }
+  }
+}
 </script>
 
 <style scoped>
