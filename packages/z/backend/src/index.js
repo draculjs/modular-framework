c073e12e13686a4263f996651eb72716058db5ba
ramiro.sandoval <ramiro_sando@outlook.com>
2023-08-07 15:01:50 -0300

    (feat): graphql upload now works with node 14 in testing module

--- packages/z/backend/src/index.js
+++ packages/z/backend/src/index.js
@@ -1,44 +1,44 @@
 require('dotenv').config();
-
-const mongoConnect = require('./mongo-db')
 import {DefaultLogger} from "@dracul/logger-backend";
+import {userCreateListener} from "@dracul/media-backend";
 import expressApp from './express-app'
 import apolloServer from './apollo-server'
 import initService from "./init/init-service";
 import defaultRoute from "./routes/DefaultRoute";
-import { userCreateListener } from '@dracul/media-backend'
+import {graphqlUploadExpress} from 'graphql-upload'
 
+const mongoConnect = require('./mongo-db')
 
-async function startApp(){
+DefaultLogger.info("Starting APP")
 
-    try{
-        DefaultLogger.info("Starting APP")
+//Connect to MongoDb
+await mongoConnect()
 
-        userCreateListener()
-        await mongoConnect()
-        //Link ApolloServer with ExpressApp
-        apolloServer.applyMiddleware({app: expressApp})
+userCreateListener()
 
-        //Default route to frontend web on monorepo strategy
-        expressApp.use(defaultRoute)
+expressApp.use(graphqlUploadExpress({maxFileSize: 1000000000, maxFiles: 10}))
 
-        await initService()
+//Link ApolloServer with ExpressApp
+apolloServer.applyMiddleware({app: expressApp})
 
-        const PORT = process.env.APP_PORT ? process.env.APP_PORT : "5000"
-        const URL = process.env.APP_API_URL ? process.env.APP_API_URL : "http://localhost" + PORT
+//Default route to frontend web on monorepo strategy
+expressApp.use(defaultRoute)
 
-        const server = expressApp.listen(PORT, () => {
-            DefaultLogger.info(`Web Server started: ${URL}`)
-            DefaultLogger.info(`Graphql Server ready: ${URL}${apolloServer.graphqlPath}`)
-        })
-        server.setTimeout(420000);
-    }catch (error) {
-        DefaultLogger.error(error.message, error)
-    }
+//initialize permissions, roles, users, customs, seeds
+initService()
+    //After initialize start to listen request
+    .then(() => {
 
+    const PORT = process.env.APP_PORT ? process.env.APP_PORT : "5000"
+    const URL = process.env.APP_API_URL ? process.env.APP_API_URL : "http://localhost" + PORT
 
-}
+    const server = expressApp.listen(PORT, () => {
+        DefaultLogger.info(`Web Server started: ${URL}`)
+        DefaultLogger.info(`Graphql Server ready: ${URL}${apolloServer.graphqlPath}`)
+    })
+    server.setTimeout(420000);
 
-
-startApp()
+}).catch(err => {
+    DefaultLogger.error(err.message, err)
+})
 
