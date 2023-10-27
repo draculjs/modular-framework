f7e9579d01fdf276f94010c65c96f5b71b42d48e
ramirosandoval <ramiro_sando@outlook.com>
2023-10-27 13:42:20 -0300

    fix(audit-backend): paginateAudit is exported

--- packages/audit/audit-backend/src/index.js
+++ packages/audit/audit-backend/src/index.js
@@ -1,13 +1,13 @@
 import { types, resolvers } from "./graphql/index.js"
 import * as permissions from "./permissions/AuditPermissions.js"
 import AuditModel from "./models/AuditModel.js"
-import { createAudit, updateAudit, fetchAudit, deleteAudit } from "./services/AuditService.js"
+import { createAudit, fetchAudit, paginateAudit, deleteAudit } from "./services/AuditService.js"
 
 export {
     //Services
     createAudit,
-    updateAudit,
     fetchAudit,
+    paginateAudit,
     deleteAudit,
     //Types and resolvers
     types,
