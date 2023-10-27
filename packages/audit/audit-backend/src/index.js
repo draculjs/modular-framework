8889edfb746c9c2359088cc9cf57bba16e633274
Cristian Incarnato <cristian.cdi@gmail.com>
2023-10-27 13:56:21 -0300

    fix: remove export auditDelete

--- packages/audit/audit-backend/src/index.js
+++ packages/audit/audit-backend/src/index.js
@@ -1,14 +1,13 @@
 import { types, resolvers } from "./graphql/index.js"
 import * as permissions from "./permissions/AuditPermissions.js"
 import AuditModel from "./models/AuditModel.js"
-import { createAudit, fetchAudit, paginateAudit, deleteAudit } from "./services/AuditService.js"
+import { createAudit, fetchAudit, paginateAudit } from "./services/AuditService.js"
 
 export {
     //Services
     createAudit,
     fetchAudit,
     paginateAudit,
-    deleteAudit,
     //Types and resolvers
     types,
     resolvers,
