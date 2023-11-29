fb724bd1675e912419aa99cac3dabbda07a0e3b7
ramirosandoval <ramiro_sando@outlook..com>
2023-11-29 14:08:03 -0300

    feat(storeFile): created and used new custom error class MaxFileSizeExceededError. Also, the file was refactored to use modern async await syntax and is documented with JSDOC

--- packages/media/media-backend/src/services/helpers/storeFile.js
+++ packages/media/media-backend/src/services/helpers/storeFile.js
@@ -1,36 +1,63 @@
+/**
+ * @fileOverview
+ * This module provides a function to store a file, performing size validation and error handling.
+ * @module FileStorage
+ */
+
+import { DefaultLogger as winston } from '@dracul/logger-backend';
+import { findUserStorageByUser } from "../UserStorageService";
+import createDirIfNotExist from './createDirIfNotExist';
+
+import { Transform, pipeline } from 'stream';
+import util from "util";
 import fs from "fs";
-import {Transform} from 'stream'
-import {DefaultLogger as winston} from '@dracul/logger-backend';
-import {findUserStorageByUser} from "../UserStorageService";
 
+/**
+ * Custom error class for when the maximum file size is exceeded.
+ * @class
+ * @extends Error
+ * @param {string} message - The error message.
+ */
+class MaxFileSizeExceededError extends Error {
+    constructor(message) {
+        super(message)
+        this.code = 'MAX_FILE_SIZE_EXCEEDED'
+    }
+}
 
-const createDirIfNotExist = require('./createDirIfNotExist')
-
+/**
+ * A Transform stream to validate the size of the incoming data chunk.
+ * @class
+ * @extends Transform
+ * @param {number} maxFileSize - The maximum allowed file size in megabytes.
+ * @param {number} storageLeft - The available storage space in megabytes.
+ */
 class StreamSizeValidator extends Transform {
-
     constructor(maxFileSize, storageLeft) {
-        super();
+        super()
         this.maxFileSize = maxFileSize
         this.storageLeft = storageLeft
         this.totalLength = 0
         this.error = ''
     }
 
-
-    _transform(chunk, encoding, callback) {
-
+    /**
+     * Transform function to validate the size of each incoming chunk.
+     * @param {Buffer} chunk - The data chunk to be validated.
+     * @param {string} _encoding - The encoding of the chunk (not used in this implementation).
+     * @param {Function} callback - The callback function to signal the completion of the transform.
+     */
+    _transform(chunk, _encoding, callback) {
         this.totalLength += chunk.length / (1024 * 1024)
+
         if (this.totalLength > this.maxFileSize) {
-            this.error = 'MAX_FILE_SIZE_EXCEEDED';
-            winston.error("storeFile.StreamSizeValidator: _transform error: " + this.error)
-            this.destroy(new Error(this.error));
-            return
+            this.error = new MaxFileSizeExceededError("File upload failed. The uploaded file exceeds the maximum allowed size. Please ensure your file is within the accepted size limit of " + this.maxFileSize + " MBs.")
+            this.destroy(this.error)
         }
+
         if (this.totalLength > this.storageLeft) {
-            this.error = 'STORAGE_CAPACITY_EXCEEDED';
-            winston.error("storeFile.StreamSizeValidator: _transform error: " + this.error)
-            this.destroy(new Error(this.error));
-            return
+            this.error = 'STORAGE_CAPACITY_EXCEEDED'
+            this.destroy(new Error("Storage capacity exceeded; available storage: " + this.storageLeft))
         }
 
         this.push(chunk)
@@ -38,61 +65,44 @@ class StreamSizeValidator extends Transform {
     }
 }
 
+/**
+ * Asynchronously stores a file, performing size validation and utilizing user-specific storage settings.
+ * @async
+ * @function
+ * @param {ReadableStream} fileStream - The readable stream of the file to be stored.
+ * @param {string} dst - The destination path where the file will be stored.
+ * @param {object} user - The user object associated with the file.
+ * @throws {Error} Throws an error if any validation or storage operation fails.
+ * @returns {object} An object indicating the success of the operation and the number of bytes written.
+ * @property {boolean} finish - Indicates whether the file storage process completed successfully.
+ * @property {number} bytesWritten - The number of bytes written to the destination.
+ */
+export default async function storeFile(fileStream, dst, user) {
+    try {
+        if (!fileStream || !fileStream.readable) throw new Error("A readable stream is required")
+        if (typeof fileStream.pipe !== 'function') throw new Error("Stream needs the pipe method")
+        if (!user) throw new Error("user parameter was not provided to storeFile")
 
-const storeFile = function (fileStream, dst, user) {
-    if (!fileStream.readable) {
-        throw new Error("A redeable stream is required")
-    }
+        let maxFileSize = process.env.MEDIA_MAX_SIZE_PER_FILE_IN_MEGABYTES
+        let storageLeft = maxFileSize
 
-    if (typeof fileStream.pipe != 'function') {
-        throw new Error("Stream needs the pipe method")
-    }
-
-    return new Promise(async (resolve, reject) => {
-
-            let maxFileSize = process.env.MEDIA_MAX_SIZE_PER_FILE_IN_MEGABYTES
-            let storageLeft = maxFileSize
-
-            if (user) {
-                const userStorage = await findUserStorageByUser(user)
-                if (userStorage) {
-                    storageLeft = userStorage.capacity - userStorage.usedSpace
-                    maxFileSize = userStorage.maxFileSize
-                }
-            }
-
-
-            const sizeValidator = new StreamSizeValidator(maxFileSize,storageLeft)
-
-            createDirIfNotExist(dst)
-
-            const fileWriteStream = fs.createWriteStream(dst)
-
-            fileStream
-                .on('error', error => {
-
-                    if (fileStream.truncated) {
-                        fs.unlinkSync(dst);
-                    }
-
-                    return reject(error);
-                })
-
-            sizeValidator
-                .on("error", error => {
-                    return reject(error)
-                })
-
-            fileStream
-                .pipe(sizeValidator)
-                .pipe(fileWriteStream)
-                .on('error', error => reject(error))
-                .on('finish', () => {
-                    resolve({finish: true, bytesWritten: fileWriteStream.bytesWritten})
-                })
+        const userStorage = await findUserStorageByUser(user)
+        if (userStorage) {
+            storageLeft = userStorage.capacity - userStorage.usedSpace
+            maxFileSize = userStorage.maxFileSize
         }
-    );
-};
 
-module.exports.storeFile = storeFile
-module.exports = storeFile
+        const sizeValidator = new StreamSizeValidator(maxFileSize, storageLeft)
+
+        createDirIfNotExist(dst)
+        const fileWriteStream = fs.createWriteStream(dst)
+
+        const asyncPipeline = util.promisify(pipeline)
+        await asyncPipeline(fileStream, sizeValidator, fileWriteStream)
+
+        return { finish: true, bytesWritten: fileWriteStream.bytesWritten }
+    } catch (error) {
+        winston.error("An error happened at the storeFile function:", error)
+        throw error
+    }
+}
