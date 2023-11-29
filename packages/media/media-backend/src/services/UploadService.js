e1e0cf26dbb9ddd49dc7c43ddefc68bab91e52f9
ramirosandoval <ramiro_sando@outlook..com>
2023-11-29 14:05:11 -0300

    chore(UploadService): refactored fileUpload function to use modern async await syntax

--- packages/media/media-backend/src/services/UploadService.js
+++ packages/media/media-backend/src/services/UploadService.js
@@ -1,93 +1,87 @@
+import { updateUserUsedStorage, findUserStorageByUser } from './UserStorageService';
+import randomString from './helpers/randomString';
+import storeFile from './helpers/storeFile';
+import baseUrl from "./helpers/baseUrl";
+import File from '../models/FileModel';
+
 import { DefaultLogger as winston } from '@dracul/logger-backend';
 import path from "path";
-import File from '../models/FileModel'
-import storeFile from './helpers/storeFile'
-import randomString from './helpers/randomString'
-import baseUrl from "./helpers/baseUrl";
-import { updateUserUsedStorage, findUserStorageByUser } from './UserStorageService';
 
-const fileUpload = function (user, inputFile, expirationDate, isPublic = false, description, tags, groups, users) {
-  return new Promise(async (resolve, rejects) => {
-    try {
 
-      if (!user) return rejects(new Error("user is required"))
+const fileUpload = async function (user, inputFile, expirationDate, isPublic = false, description, tags, groups, users) {
+  try {
+    if (!user) throw new Error("user is required")
 
-      let { filename, mimetype, encoding, createReadStream } = await inputFile
-      filename = filename.replaceAll(' ', '_')
-      let type = mimetype.split("/")[0]
+    const { filename, mimetype, encoding, createReadStream } = await inputFile
+    filename = filename.replaceAll(' ', '_')
+    let type = mimetype.split("/")[0]
 
-      const parseFileName = path.parse(filename);
-      const extension = parseFileName.ext
-      const name = parseFileName.name.replace(/#/g, "")
-      const hash = '-' + randomString(6)
-      const finalFileName = name + hash + extension
-      const year = new Date().getFullYear().toString()
-      const month = (new Date().getMonth() + 1).toString()
-      const relativePath = path.join("media", "files", user.username, year, month, finalFileName)
-      const absolutePath = path.resolve(relativePath);
+    const parseFileName = path.parse(filename);
+    const extension = parseFileName.ext
+    const name = parseFileName.name.replace(/#/g, "")
+    const hash = '-' + randomString(6)
+    const finalFileName = name + hash + extension
+    const year = new Date().getFullYear().toString()
+    const month = (new Date().getMonth() + 1).toString()
+    const relativePath = path.join("media", "files", user.username, year, month, finalFileName)
+    const absolutePath = path.resolve(relativePath);
 
-      //Store
-      let storeResult = await storeFile(createReadStream(), relativePath, user)
-      winston.info("fileUploadAnonymous store result: " + storeResult)
+    const storeResult = await storeFile(createReadStream(), relativePath, user)
+    winston.info("fileUploadAnonymous store result: " + storeResult)
 
-      if (expirationDate) {
-        let timeDiffExpirationDate = validateExpirationDate(expirationDate)
+    if (expirationDate) {
+      let timeDiffExpirationDate = validateExpirationDate(expirationDate)
 
-        if (!timeDiffExpirationDate) {
-          winston.error("Expiration date must be older than current date")
-          return rejects(new Error("Expiration date must be older than current date"))
-        }
-
-        let userStorage = await findUserStorageByUser(user)
-
-        if (timeDiffExpirationDate > userStorage.fileExpirationTime) {
-          winston.error(`File expiration can not be longer than max user expiration time per file (${userStorage.fileExpirationTime} days)`)
-          return rejects(new Error(`File expiration can not be longer than max user expiration time per file (${userStorage.fileExpirationTime} days)`))
-        }
+      if (!timeDiffExpirationDate) {
+        winston.error("Expiration date must be older than current date")
+        throw new Error("Expiration date must be older than current date")
       }
 
-      let url = baseUrl() + relativePath
+      let userStorage = await findUserStorageByUser(user)
 
-      if (storeResult && storeResult.finish) {
-
-        let fileSizeMB = storeResult.bytesWritten / (1024 * 1024)
-
-        updateUserUsedStorage(user.id, fileSizeMB)
-
-        let doc = new File({
-          filename: finalFileName,
-          mimetype: mimetype,
-          encoding: encoding,
-          type: type,
-          extension: extension,
-          relativePath: relativePath,
-          absolutePath: absolutePath,
-          size: fileSizeMB,
-          url: url,
-          createdBy: { user: user.id, username: user.username },
-          expirationDate: expirationDate,
-          isPublic: isPublic,
-          description: description,
-          tags: tags,
-          groups: groups,
-          users: users
-        });
-        winston.info("fileUploadAnonymous saving file")
-        await doc.save()
-        winston.info("fileUploadAnonymous file saved: " + doc._id)
-
-        doc.populate('createdBy.user').execPopulate(() => (resolve(doc)))
-
-      } else {
-        winston.error("Upload Fail")
-        return  rejects(new Error("Upload Fail"))
+      if (timeDiffExpirationDate > userStorage.fileExpirationTime) {
+        winston.error(`File expiration can not be longer than max user expiration time per file (${userStorage.fileExpirationTime} days)`)
+        throw new Error(`File expiration can not be longer than max user expiration time per file (${userStorage.fileExpirationTime} days)`)
       }
-
-    } catch (err) {
-      winston.error('UploadService error' + err)
-      return  rejects(err)
     }
-  })
+
+    let url = baseUrl() + relativePath
+
+    if (storeResult && storeResult.finish) {
+
+      let fileSizeMB = storeResult.bytesWritten / (1024 * 1024)
+
+      updateUserUsedStorage(user.id, fileSizeMB)
+
+      let doc = new File({
+        filename: finalFileName,
+        mimetype: mimetype,
+        encoding: encoding,
+        type: type,
+        extension: extension,
+        relativePath: relativePath,
+        absolutePath: absolutePath,
+        size: fileSizeMB,
+        url: url,
+        createdBy: { user: user.id, username: user.username },
+        expirationDate: expirationDate,
+        isPublic: isPublic,
+        description: description,
+        tags: tags,
+        groups: groups,
+        users: users
+      })
+
+      await doc.save()
+      winston.info("fileUploadAnonymous file saved: " + doc._id)
+      return await doc.populate('createdBy.user').execPopulate()
+
+    }
+
+  } catch (error) {
+    winston.error('An error happened at uploadService: ' + error)
+    throw error
+  }
 
 }
 
