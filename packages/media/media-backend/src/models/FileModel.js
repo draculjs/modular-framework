a77761c14855d8e7ed38c2bc568a062e62120af2
ramirosandoval <ramiro_sando@outlook..com>
2026-03-19 15:27:38 -0300

    fix(media-backend): cadd compound indexes for policy-based expiration queries

--- packages/media/media-backend/src/models/FileModel.js
+++ packages/media/media-backend/src/models/FileModel.js
@@ -21,7 +21,7 @@ const FileSchema = new Schema({
     absolutePath: { type: String, required: true },
     size: { type: Number, required: true },
     url: { type: String, required: true },
-    lastAccess: { type: Date, required: true, default: Date.now },
+    lastAccess: { type: Date, required: true, default: Date.now, index: true },
     createdAt: { type: Date, required: true, default: Date.now, index: true },
     createdBy: {
         user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false, index: true },
@@ -38,6 +38,11 @@ const FileSchema = new Schema({
 });
 
 FileSchema.plugin(mongoosePaginate)
+
+FileSchema.index({ expirationDate: 1, createdAt: 1 })
+FileSchema.index({ "createdBy.user": 1, expirationDate: 1 })
+FileSchema.index({ expirationDate: 1, "createdBy.user": 1, lastAccess: 1 })
+FileSchema.index({ expirationDate: 1, "createdBy.user": 1, createdAt: 1 })
 
 const File = mongoose.model('File', FileSchema)
 
