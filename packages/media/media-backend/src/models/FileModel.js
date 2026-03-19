cb5b4969626ea2f39057c739d84e11e39144a203
ramirosandoval <ramiro_sando@outlook..com>
2026-03-19 08:55:03 -0300

    se agregan indices a FileModel

--- packages/media/media-backend/src/models/FileModel.js
+++ packages/media/media-backend/src/models/FileModel.js
@@ -22,12 +22,12 @@ const FileSchema = new Schema({
     size: { type: Number, required: true },
     url: { type: String, required: true },
     lastAccess: { type: Date, required: true, default: Date.now },
-    createdAt: { type: Date, required: true, default: Date.now },
+    createdAt: { type: Date, required: true, default: Date.now, index: true },
     createdBy: {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false, index: true },
         username: { type: String, required: true }
     },
-    expirationDate: { type: Date, required: false },
+    expirationDate: { type: Date, required: false, index: true },
     isPublic: { type: Boolean, required: false },
     hits: { type: Number, require: false, default: 0 },
     groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group", required: false }],
@@ -38,6 +38,7 @@ const FileSchema = new Schema({
 });
 
 FileSchema.plugin(mongoosePaginate)
+
 const File = mongoose.model('File', FileSchema)
 
 module.exports = File
